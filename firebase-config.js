e55d37677cf9b1a24f96f4c15f80cce68c168446
deives <davidbreno810@gmail.com>
2025-10-08 13:30:57 -0300

    Remove arquivos desnecessários: .gitignore e publish-to-github.ps1; adiciona configuração do Firebase.

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAHl828nbtjx48d3vkVY7m00HlEigOD5mo",
+  authDomain: "notepad-online-70ee8.firebaseapp.com",
+  projectId: "notepad-online-70ee8",
+  storageBucket: "notepad-online-70ee8.firebasestorage.app",
+  messagingSenderId: "830573157477",
+  appId: "1:830573157477:web:aed74d6fa04db30171ba8c"
+};
